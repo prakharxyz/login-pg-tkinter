e6a3024b529582f74a4cee617d6d5408c3b3849b
prakhar <prkhrgrwl@gmail.com>
2022-01-21 15:33:57 +0530

    login pg 21/01

--- login_page_new.py
+++ login_page_new.py
@@ -1,46 +1,56 @@
 
-from asyncio.windows_events import NULL
 from tkinter import *
-from turtle import onclick, width
+from tkinter import messagebox
+from PIL import ImageTk,Image
 
 class GUI(Tk):
     def __init__(self):
         super().__init__()
-        self.geometry('400x300')
+        self.geometry('700x500')
         self.title('Login page')
+        self.primaryColor = '#F5FFFA'
+        self.secondaryColor = '#151B54'
         self.emailId = StringVar()
         self.password = StringVar()
+        self.resizable(False,False)
+        # self.canvas1 = Canvas(self)
+        # self.canvas1.pack()
 
 
     def createFrame(self):
-        self.f = Frame(self, height='100', width='150')
-        self.f.pack()
+        self.f = Frame(self, height='300', width='400', bg=self.primaryColor,highlightthickness=1, relief='raised', bd=2)
+        self.f.config(highlightbackground = self.secondaryColor, highlightcolor= self.secondaryColor)
+        self.f.place(x='190', y='110', width='350', height='275')
 
     def createHeading(self, ltext, lfont):
-        self.label = Label(self.f, text=ltext, font= lfont, padx= 15, pady= 25, justify=CENTER)
-        self.label.pack()
+        self.label = Label(self.f, text=ltext, font= lfont, padx= 15, pady= 25, bg=self.primaryColor, fg=self.secondaryColor)
+        self.label.place(x='125', y='10')
 
-    def createLabel(self, ltext, lfont, ljustify, lpadx, lpady):
-        self.label = Label(self.f, text=ltext, font= lfont, padx= lpadx, pady= lpady, justify=ljustify)
-        self.label.pack(anchor='sw', pady=(10,0))
+    def createLabel(self, ltext, lfont, xx, yy):
+        self.label = Label(self.f, text=ltext, font= lfont, padx= 2, pady= 2, bg=self.primaryColor, fg=self.secondaryColor)
+        self.label.place(x=xx, y=yy)
 
-    def createField(self):
-        self.field = Entry(self.f, textvariable=self.emailId, justify=CENTER)
-        self.field.pack()
+    def createField(self, xx, yy):
+        self.field = Entry(self.f, textvariable=self.emailId)
+        self.field.place(x=xx, y=yy)
         self.field.insert(1, 'username')
 
-    def createObscureField(self):
-        self.field = Entry(self.f, textvariable=self.password, justify=CENTER, show='*')
-        self.field.pack()
+    def createObscureField(self, xx, yy):
+        self.field = Entry(self.f, textvariable=self.password, show='*')
+        self.field.place(x=xx, y=yy)
 
     def createButton(self, btext):
-        self.button = Button(self.f, command=self.print, text=btext)
-        self.button.pack(padx='10', pady='10')
+        self.button = Button(self.f, command=self.print, text=btext, bg=self.secondaryColor, fg=self.primaryColor)
+        self.button.place(x='125', y='210')
         
     def print(self):
         print('ok')
         print(f'E-mail - {self.emailId.get()}')
         print(f'Password - {self.password.get()}')
+        if self.emailId.get() == 'prakhar@gmail.com' and self.password.get() == '12345678':
+            messagebox.showinfo('Login Successful', 'You have been successfully logged in to prakhar@gmail.com')
+        else:
+            messagebox.showinfo('Login Failed', 'Invalid username or password')
 
     
 
@@ -50,10 +60,10 @@ if __name__ == '__main__':
 
     window.createFrame()
     window.createHeading('LOGIN', 'consolas 18 bold')
-    window.createLabel('Username', 'arial 9 ', LEFT, 2, 2,)
-    window.createField()
-    window.createLabel('Password', 'arial 9 ', LEFT, 2, 2)
-    window.createObscureField()
+    window.createLabel('Username', 'arial 9 ', 70 ,110)
+    window.createField(150, 110)
+    window.createLabel('Password', 'arial 9 ', 70, 160)
+    window.createObscureField(150, 160)
     window.createButton('Submit')
 
     window.mainloop()
